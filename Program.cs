f0e10e73363fa4ac839238225b984b58eff8b94f
Omar Ibne Alam <83441944+Omar035@users.noreply.github.com>
2023-10-07 12:30:50 +0600

    Improvements

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace CSE_Lab_2104076_GeoShapes
+﻿using System.Net;
+
+namespace CSE_Lab_2104076_GeoShapes
 {
     class Shape
     {
         public virtual double Area()
         {
             return 0;
+        }
+
+        public virtual bool IsValid () {
+            return true;
         }
     }
 
@@ -16,23 +22,32 @@
             return ((1 / 4) * (22/7) * diameter * diameter);
         }
 
+        public override bool IsValid()
+        {
+            if (diameter > 0) {
+                return true;
+            } else {
+                return false;
+            }
+            
+        }
         public Circle ()
         {
-            while (true)
-            {
-                Console.Write("\nEnter the diameter of the circle: ");
-                diameter = Convert.ToDouble(Console.ReadLine());
+            bool validInput = false;
+            
+            while (!validInput) {
+                try {
+                    Console.Write("\nEnter the diameter of the circle: ");
+                    diameter = Convert.ToDouble(Console.ReadLine());
+                    validInput = true;
 
-                if (diameter < 0)
-                {
-                    Console.WriteLine("Invalid Input!");
-                    continue;
+                    Console.WriteLine("\nAdded a circle");
+                } catch (FormatException ex) {
+                    Console.WriteLine(ex.Message);
+                } catch (OverflowException ex) {
+                    Console.WriteLine(ex.Message);
                 }
-
-                break;
             }
-
-            Console.WriteLine("\nSuccessfully added a circle");
         }
     }
 
@@ -40,6 +55,7 @@
     {
         public double length { get; set; }
         public double width { get; set; }
+        public double angle1, angle2, angle3, angle4;
 
         public override double Area ()
         {
@@ -48,35 +64,47 @@
 
         public Rectangle ()
         {
-            while (true)
-            {
-                Console.Write("\nEnter length: ");
-                length = Convert.ToDouble(Console.ReadLine());
 
-                if (length < 0)
-                {
-                    Console.WriteLine("Invalid input!");
-                    continue;
+            bool validInput = false;
+
+            while (!validInput) {
+                try {
+                    Console.Write("\nEnter length: ");
+                    length = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter width: ");
+                    width = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Enter angle-1: ");
+                    angle1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Enter angle-2: ");
+                    angle2 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Enter angle-3: ");
+                    angle3 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Enter angle-4: ");
+                    angle4 = Convert.ToDouble(Console.ReadLine());
+
+                    validInput = true;
+                } catch (FormatException ex) {
+                    Console.WriteLine(ex.Message);
+                } catch (OverflowException ex) {
+                    Console.WriteLine(ex.Message);
                 }
-
-                break;
             }
 
-            while (true)
-            {
-                Console.Write("Enter width: ");
-                width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nAdded a rectangle.");
+        }
 
-                if (width < 0)
-                {
-                    Console.WriteLine("Invalid input!");
-                    continue;
-                }
-
-                break;
+        public override bool IsValid()
+        {
+            if ((length > 0) && (width > 0) && (angle1 + angle2 + angle3 + angle4 == 360)) {
+                return true;
+            } else {
+                return false;
             }
-
-            Console.WriteLine("\nSuccessfully added a rectangle.");
         }
     }
 
@@ -90,20 +118,31 @@
 
         public Cube ()
         {
-            while (true)
-            {
-                Console.Write("Enter side of the cube: ");
-                side = Convert.ToDouble(Console.ReadLine());
+            bool validInput = false;
+            
+            while (!validInput) {
+                try {
+                    Console.Write("Enter side of the cube: ");
+                    side = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nAdded a cube");
 
-                if (side < 0)
-                {
-                    Console.WriteLine("Invalid input!");
-                    continue;
+                    validInput = true;
+
+                } catch (FormatException ex) {
+                    Console.WriteLine(ex.Message);
+                } catch (OverflowException ex) {
+                    Console.WriteLine(ex.Message);
                 }
-
-                break;
             }
-            Console.WriteLine("\nSucccessfully added a cube");
+        }
+
+        public override bool IsValid()
+        {
+            if (side > 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
     }
@@ -122,64 +161,113 @@
 
         public Triangle ()
         {
-            while (true)
-            {
-                while (true)
-                {
-                    Console.Write("Enter side 1: ");
+            bool validInput = false;
+
+            while (!validInput) {
+                try {
+                    Console.WriteLine("Enter side-1: ");
                     side1 = Convert.ToDouble(Console.ReadLine());
 
-                    if (side1 < 0)
-                    {
-                        Console.WriteLine("Invalid input!");
-                        continue;
-                    }
-
-                    break;
-                }
-
-                while (true)
-                {
-                    Console.Write("Enter side 2: ");
+                    Console.WriteLine("Enter side-2: ");
                     side2 = Convert.ToDouble(Console.ReadLine());
 
-                    if (side2 < 0)
-                    {
-                        Console.WriteLine("Invalid input");
-                        continue;
-                    }
-
-                    break;
-                }
-
-                while (true)
-                {
-                    Console.Write("Enter side 3: ");
+                    Console.WriteLine("Enter side-3: ");
                     side3 = Convert.ToDouble(Console.ReadLine());
 
-                    if (side3 < 0)
-                    {
-                        Console.WriteLine("Invalid input");
-                        continue;
-                    }
+                    Console.WriteLine("Added a Triangle!");
 
-                    break;
-                }
-
-                if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2) {
-                    Console.WriteLine("\nSucccessfully added a triangle");
-                    break;
-                } else
-                {
-                    Console.WriteLine("Side inputs cannot construct a triangle! Enter side again!");
+                    validInput = true;
+                } catch (FormatException ex) {
+                    Console.WriteLine(ex.Message);
+                } catch (OverflowException ex) {
+                    Console.WriteLine(ex.Message);
                 }
             }
         }
     }
 
+    class WHManager {
+        private Dictionary<string, List<Shape>> warehouse =  new ();
+        public void AddShapeObject(string str, Shape s)
+        {
+            if (!warehouse.ContainsKey(str))
+            {
+                warehouse[str] = new ();
+            }
+
+            try
+            {
+                if (s.IsValid())
+                {
+                    warehouse[str].Add(s);
+                    Console.WriteLine("\nShape added successfully.");
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid shape. Not added to the warehouse.");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void ListItems()
+        {
+            Console.WriteLine("\n\t\tItems in the warehouse:");
+
+            for (int i = 0; i < 70; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.Write("\n");
+
+            Console.Write("Circles: ");
+
+            foreach (var c in warehouse["circle"]) {
+                Console.Write(((Circle)c).diameter + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Rectangles: ");
+            foreach (var r in warehouse["rectangle"]) {
+                Console.Write(((Rectangle)r).length + "x" + ((Rectangle)r).width + " ");    
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Cubes: ");
+            foreach (var c in warehouse["cube"]) {
+                Console.Write(((Cube)c).side + "x" + ((Cube)c).side + "x" + ((Cube)c).side + " ");    
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Triangles: ");
+            foreach (var t in warehouse["triangle"]) {
+                Console.Write(((Triangle)t).Area() + " ");
+            }
+
+            Console.WriteLine();
+
+        }
+
+        public void GetStats()
+        {
+            Console.WriteLine("Total no. of Shapes: " + (warehouse["circle"].Count + warehouse["rectangle"].Count + warehouse["cube"].Count + warehouse["triangle"].Count));
+
+            Console.WriteLine("Circles: " + warehouse["circle"].Count);
+            Console.WriteLine("Rectangles: " + warehouse["rectangle"].Count);
+            Console.WriteLine("Cubes: " + warehouse["cube"].Count);
+            Console.WriteLine("Triangles: " + warehouse["triangle"].Count);
+        }
+    }
+
     class Driver
     {
-        public static List <Shape> Warehouse = new List <Shape> ();
         public static void Main()
         {
             Console.WriteLine("\t\tWelcome to Shapes' Warehouse!");
@@ -193,6 +281,8 @@
 
             int choice;
 
+            WHManager mg = new();
+
             while (true)
             {
                 Console.WriteLine("\n[1] Add a Circle");
@@ -202,90 +292,46 @@
                 Console.WriteLine("[5] List Items");
                 Console.WriteLine("[6] Get Statistics");
                 Console.WriteLine("[7] Exit");
-                Console.Write("Enter your choice: "); 
+                Console.Write("Enter your choice: ");
 
                 choice = Convert.ToInt16(Console.ReadLine());
 
-                switch (choice)
-                {
-                    case 1:
-                        Circle circle = new Circle();
-                        Warehouse.Add(circle);
-                        continue;
-                    case 2:
-                        Rectangle rectangle = new Rectangle();
-                        Warehouse.Add(rectangle);
-                        continue;
-                    case 3:
-                        Cube cube = new Cube();
-                        Warehouse.Add(cube);
-                        continue;
-                    case 4:
-                        Triangle triangle = new Triangle();
-                        Warehouse.Add(triangle);
-                        continue;
-                    case 5:
-                        ListItems();
-                        continue;
-                    case 6:
-                        GetStats();
-                        continue;
-                    case 7:
-                        Console.WriteLine("\nSuccessfully exited the program.");
-                        break;
+                try {
+                    switch (choice)
+                    {
+                        case 1:
+                            Circle circle = new ();
+                            mg.AddShapeObject("circle", circle);
+                            continue;
+                        case 2:
+                            Rectangle rectangle = new ();
+                            mg.AddShapeObject("rectangle", rectangle);
+                            continue;
+                        case 3:
+                            Cube cube = new ();
+                            mg.AddShapeObject("cube", cube);
+                            continue;
+                        case 4:
+                            Triangle triangle = new ();
+                            mg.AddShapeObject("triangle", triangle);
+                            continue;
+                        case 5:
+                            mg.ListItems();
+                            continue;
+                        case 6:
+                            mg.GetStats();
+                            continue;
+                        case 7:
+                            Console.WriteLine("\nSuccessfully exited the program.");
+                            break;
+                    }
+                } catch (KeyNotFoundException) {
+                    Console.WriteLine("Desired shapes don't yet exist in the warehouse!\n");
+                    continue;
                 }
 
                 break;
             }
-        }
-
-        public int TotalShapes = Warehouse.Count();
-        
-
-        private static void ListItems()
-        {
-            Console.WriteLine("\n\t\tItems in the warehouse");
-
-            for (int i = 0; i < 70; i++)
-            {
-                Console.Write("-");
-            }
-
-            Console.Write("\n");
-
-            Console.WriteLine("\tID\t\t\tType\t\t\tDimension\n");
-
-            for (int i = 0; i < 70; i++)
-            {
-                Console.Write("-");
-            }
-
-            Console.Write('\n');
-
-            int index = 1;
-
-            foreach (var item in Warehouse)
-            {
-                if (item is Circle)
-                {
-                    Console.WriteLine("\t" + index + "\t\t\tCircle\t\t\t" + ((Circle)item).diameter);
-                } else if (item is Rectangle)
-                {
-                    Console.WriteLine("\t" + index + "\t\t\tRectangle\t\t\t" + ((Rectangle)item).length + " x " + ((Rectangle)item).width);
-                } else if (item is Cube) {
-                    Console.WriteLine("\t" + index + "\t\t\tCube\t\t\t" + ((Cube)item).side + " x " + ((Cube)item).side + " x " + (((Cube)item).side));
-                } else {
-                    
-                }
-
-                index++;
-            }
-
-        }
-
-        private static void GetStats()
-        {
-            throw new NotImplementedException ();
         }
     }
 }
